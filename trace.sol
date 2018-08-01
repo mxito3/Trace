e8d4e839fd048f62f3d551bee5d55382b715d632
mxito3 <1477311729@qq.com>
2018-08-01 12:06:11 +0800

    rewrite in a more efficient way

--- trace.sol
+++ trace.sol
@@ -1,55 +1,52 @@
 pragma solidity ^0.4.24;
 
-struct feed  //饲料
-{    
-    uint name;
-    //string[] ingredent;//成分 
-}
 
-struct waterQuality  //水质
-{
-    bool whetherQualified;
-    string checkAgent;
-    uint animalDensity;//蟹苗密度
-}
-
-
-struct  transferInformation 
-{
-    string from; //出发地
-    string to;  //下一站       
-}
-
-struct storeInformation
-{
-    uint temperature;
-    uint wetness;
-}
-struct changeTimeAndOperator
+contract baseInformation
 {
     uint time;
     string opratorName;
+    function addTimeAndOperator(strign _opratorName) external
+    {
+        opratorName=_opratorName;
+        time=now;
+    }
+}
+
+contract feed {
+      uint name; //基础信息
+      baseInformation base;//基础信息  has a的关系
+}
+
+contract waterQuality{
+    bool whetherQualified;
+    string checkAgent;
+    uint animalDensity;//蟹苗密度
+    baseInformation base;//基础信息  has a的关系
+}
+contract transfer{
+    string from; //出发地
+    string to;  //下一站   
+    baseInformation base;//基础信息  has a的关系
+}
+contract store{
+    uint temperature;
+    uint wetness;
+    baseInformation base;//基础信息  has a的关系
 }
 
 contract crab
-{
-     
+{ 
     uint id;
+    baseInformation initCrab;//初始加入时间
     feed[] feed;
     waterQuality[] waterQuality;
     transferInformation[] transferInformation;
     storeInformation[] storeInformation;
-    changeTimeAndOperator addcrab;
-    changeTimeAndOperator[] changeFeed;  //饲料信息  
-    changeTimeAndOperator[] changeWaterQuality;  //水质信息
-    changeTimeAndOperator[] changeProtein;  //蛋白质信息
-    changeTimeAndOperator[] changeTransfer;     //转运信息
-    changeTimeAndOperator[] changeStore;    //存储信息
     function crab (uint _id) returns(bool res) internal {
         id=_id;
         return true;
     }
-    
+
 }
 
 contract Trace {
@@ -57,58 +54,46 @@ contract Trace {
  
     mapping (uint => crab) crabs;
     mapping (uint => bool) exist;
-    
+
+    modifier onlyOwner(address userAddress) { 
+        require (msg.sender == userAddress); 
+        _; 
+    }
+
     function addcrab (uint _id,string opratorName) returns(bool res) internal {
         //判断是不是存在了
         crabs[_id]=new crab(_id);
-        addTimeAndOperator(1,_id,opratorName);
         exist[_id]=true;
+        crabs[_id].initCrab.addTimeAndOperator(opratorName);
         return true;
     }
-    function changeFeed (uint _id,uint name,string opratorName) returns(bool res) internal {
+    function changeFeed (uint _id,uint feedName,string opratorName) returns(bool res) public 
+    {
         if(!exist[_id])
         {
             return false;
         }
         else
         {
-            crabs[_id].changeFeed.add(new feed(name));
-            addTimeAndOperator(2,_id,opratorName);
+            crabs[_id].feed.add(new feed(feedName));
+            crabs[_id].feed.addTimeAndOperator(opratorName);
+            return true;
         }
     }
-    
-    function Trace () {
-        
-    }
-
-    modifier onlyOwner(address userAddress) { 
-        require (msg.sender == userAddress); 
-        _; 
-    }
-    
-    function addAnimal () returns(bool res) public onlyOwner {
-        
-    }
-    
-    function  () returns(bool res) internal {
-         
-     }
-    
-    function addTimeAndOperator (uint id,uint type,string opratorName) returns(bool res) internal 
+    function changeWaterQuality (uint _id,bool _whetherQualified,string _checkAgent,uint _animalDensity,string _opratorName) returns(bool res) public 
     {
-
-        //冗余度过高，明天优化
-        if(type ==1 )//add crab
+        if(!exist[_id])
         {
-            crabs[id].addcrab.time = now;
-            crabs[id].addcrab.opratorName=opratorName;
+            return false;
         }
-        else if( type==2 )//change or add feed message
+        else
         {
-            crabs[id].changeFeed.time=now;
-            crabs[id].changeFeed.opratorName=opratorName;
+            crabs[_id].changeWaterQuality.add(new waterQuality(_whetherQualified,_checkAgent,_animalDensity));
+            addTimeAndOperator(3,_id,opratorName);
+            return true;
         }
     }
+
     
 
 }
