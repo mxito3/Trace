6ba8981927a1fc3f2cba0b66c738edd4b09ee3fb
mxito3 <1477311729@qq.com>
2018-08-01 16:45:50 +0800

    fixed known bugs

--- trace.sol
+++ trace.sol
@@ -1,50 +1,51 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.16;
 
-
-contract baseInformation
+library crabInformation
 {
-    uint time;
-    string opratorName;
-    function addTimeAndOperator(strign _opratorName) external
-    {
-        opratorName=_opratorName;
-        time=now;
-    }
+	    struct base 
+	{
+	    uint  time ;
+	    string opratorName;
+	}
+
+	struct feed {
+	      string name; //基础信息
+	      base baseInformation;
+	}
+
+	struct waterQuality{
+	    bool whetherQualified;
+	    string checkAgent;
+	    uint animalDensity;//蟹苗密度
+	    base baseInformation;
+	}
+	struct transfer{
+	    
+	    string from; //出发地
+	    string to;  //下一站 
+	    base baseInformation;  
+	}
+	struct store{
+	    uint temperature;
+	    uint wetness;
+	    base baseInformation;
+	}
+
 }
 
-contract feed {
-      uint name; //基础信息
-      baseInformation base;//基础信息  has a的关系
-}
-
-contract waterQuality{
-    bool whetherQualified;
-    string checkAgent;
-    uint animalDensity;//蟹苗密度
-    baseInformation base;//基础信息  has a的关系
-}
-contract transfer{
-    string from; //出发地
-    string to;  //下一站   
-    baseInformation base;//基础信息  has a的关系
-}
-contract store{
-    uint temperature;
-    uint wetness;
-    baseInformation base;//基础信息  has a的关系
-}
 
 contract crab
 { 
     uint id;
-    baseInformation initCrab;//初始加入时间
-    feed[] feed;
-    waterQuality[] waterQuality;
-    transferInformation[] transferInformation;
-    storeInformation[] storeInformation;
-    function crab (uint _id) returns(bool res) internal {
+    crabInformation.base initCrab;//初始加入时间
+    crabInformation.feed[] feedInformation;
+    crabInformation.waterQuality[] waterQualityInformation;
+    crabInformation.transfer[] transferInformation;
+    crabInformation.store[] storeInformation;
+    function crab (uint _id,string _opratorName) public  {
         id=_id;
-        return true;
+        initCrab.time=now;
+        initCrab.opratorName=_opratorName;
     }
 
 }
@@ -60,14 +61,18 @@ contract Trace {
         _; 
     }
 
-    function addcrab (uint _id,string opratorName) returns(bool res) internal {
+    function Trace ()  {
+        
+    }
+    
+    function addcrab (uint _id,string _opratorName) public returns(bool res)  
+    {
         //判断是不是存在了
-        crabs[_id]=new crab(_id);
-        exist[_id]=true;
-        crabs[_id].initCrab.addTimeAndOperator(opratorName);
+        crabs[_id]=new crab(_id,_opratorName);
         return true;
     }
-    function changeFeed (uint _id,uint feedName,string opratorName) returns(bool res) public 
+
+    function changeFeed (uint _id,string _feedName,string _opratorName) public returns(bool res) 
     {
         if(!exist[_id])
         {
@@ -75,26 +80,60 @@ contract Trace {
         }
         else
         {
-            crabs[_id].feed.add(new feed(feedName));
-            crabs[_id].feed.addTimeAndOperator(opratorName);
-            return true;
-        }
-    }
-    function changeWaterQuality (uint _id,bool _whetherQualified,string _checkAgent,uint _animalDensity,string _opratorName) returns(bool res) public 
-    {
-        if(!exist[_id])
-        {
-            return false;
-        }
-        else
-        {
-            crabs[_id].changeWaterQuality.add(new waterQuality(_whetherQualified,_checkAgent,_animalDensity));
-            addTimeAndOperator(3,_id,opratorName);
+            
+            
+            crabs[_id].feedInformation.push(
+                crabInformation.feed(
+                {
+                name:_feedName,
+                baseInformation:crabInformation.base
+                ({
+                    time:now,
+                    opratorName:_opratorName
+                })
+                }
+                ));
             return true;
         }
     }
 
+    function changeWaterQuality (uint _id,bool _whetherQualified,string _checkAgent,uint _animalDensity,string _opratorName) public returns(bool res)  
+    {
+        if(!exist[_id])
+        {
+            return false;
+        }
+        else
+        {
+            crabs[_id].waterQualityInformation.push(crabInformation.waterQuality(_whetherQualified,_checkAgent,_animalDensity,crabInformation.base(now,_opratorName)));
+            return true;
+        }
+    }
     
+    function changeTransferInformation (uint _id,string from,string to,string _opratorName) public returns(bool res) 
+    {
+        if(!exist[_id])
+        {
+            return false;
+        }
+        else
+        {
+            crabs[_id].transferInformation.push(crabInformation.transferInformation(from,to,crabInformation.base(now,_opratorName)));
+            return true;
+        }
+    }
 
+    function changeStoreInformation (uint _id,uint temperature,uint wetness,string _opratorName) public returns(bool res)  
+    {
+        if(!exist[_id])
+        {
+            return false;
+        }
+        else
+        {
+            crabs[_id].storeInformation.push(crabInformation.storeInformation(temperature,wetness,crabInformation.base(now,_opratorName)));
+            return true;
+        }
+    }
 }
 
