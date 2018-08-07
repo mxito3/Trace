ce9fb88e16ffdd513d9a61ce40ddbfbc53151356
mxito3 <1477311729@qq.com>
2018-08-07 21:33:20 +0800

    change webpack config

--- InJS/webpack.config.js
+++ InJS/webpack.config.js
@@ -10,7 +10,9 @@ module.exports = {
   plugins: [
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/html/index.html', to: "index.html" }
+      { from: './app/html/index.html', to: "index.html" },
+      {from:'./app/html/pushMessage.html',to:'pushMessage.html'},
+      {from:'./app/html/showData.html',to:'showData.html'}
     ])
   ],
   module: {
